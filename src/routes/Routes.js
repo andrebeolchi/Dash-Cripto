2fd5c066265cc1ee192d4fa7946d737e8d5fb9a6
andrebeolchi <rm84163@fiap.com.br>
2022-03-26 14:15:53 -0300

    Configurando Estilos e Paleta de Cores

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,17 +1,33 @@
+
+import { makeStyles } from '@mui/styles'
 import React from 'react'
 import {
     BrowserRouter, Route, Routes
 } from 'react-router-dom'
 import Header from '../components/header/Header'
 import Index from '../pages'
+import { Color } from '../utils/ColorUtil'
 
 const Rotas = () => {
+
+    const useStyles = makeStyles(() => ({
+        App: {
+            backgroundColor: Color.backgroundColor,
+            color: Color.text,
+            minHeight: '100vh',
+        },
+    }))
+
+    const classes = useStyles()
+
     return (
         <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Index />} />
-            </Routes>
+            <div className={classes.App}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Index />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     )
 }
---
+++ src/utils/ColorUtil.js
@@ -0,0 +1,10 @@
+// export a color palette
+
+export const Color = {
+    backgroundColor: '#111',
+    text: '#fff',
+    primary: '#00bcd4',
+    error: '#f44336',
+    success: '#4caf50',
+    info: '#2196f3',
+}
